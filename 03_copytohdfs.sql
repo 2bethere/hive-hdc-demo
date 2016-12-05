199ac3cd648238307caa48c6a01cf504c08aa86e
Will Xu <2bethere@gmail.com>
2016-12-05 09:30:41 -0800

    Fixing paritition of copied tables

--- 03_copytohdfs.sql
+++ 03_copytohdfs.sql
@@ -25,22 +25,53 @@ AS SELECT * from planes
 ;
 
 
-create table flights_hdfs
+
+create table flights_hdfs (
+  DateOfFlight date,
+  DepTime  int,
+  CRSDepTime int,
+  ArrTime int,
+  CRSArrTime int,
+  UniqueCarrier string,
+  FlightNum int,
+  TailNum string,
+  ActualElapsedTime int,
+  CRSElapsedTime int,
+  AirTime int,
+  ArrDelay int,
+  DepDelay int,
+  Origin string,
+  Dest string,
+  Distance int,
+  TaxiIn int,
+  TaxiOut int,
+  Cancelled int,
+  CancellationCode varchar(1),
+  Diverted varchar(1),
+  CarrierDelay int,
+  WeatherDelay int,
+  NASDelay int,
+  SecurityDelay int,
+  LateAircraftDelay int
+) 
+PARTITIONED BY (Year int)
 STORED AS ORC
 LOCATION '/tmp/airline_orc/airline_ontime.db/flights'
-AS SELECT * from flights
+TBLPROPERTIES("orc.bloom.filter.columns"="*")
 ;
+INSERT OVERWRITE TABLE flights PARTITION(year) 
+SELECT * FROM flights;
 
 
 
-alter table airports_hdfs add constraint airports_c1 primary key (iata) disable novalidate;
-alter table airlines_hdfs add constraint airlines_c1 primary key (code) disable novalidate;
-alter table planes_hdfs add constraint planes_c1 primary key (tailnum) disable novalidate;
+alter table airports_hdfs add constraint airports_hdfs_c1 primary key (iata) disable novalidate;
+alter table airlines_hdfs add constraint airlines_hdfs_c1 primary key (code) disable novalidate;
+alter table planes_hdfs add constraint planes_hdfs_c1 primary key (tailnum) disable novalidate;
 
-alter table flights add constraint flights_new_c1 foreign key (Origin) references airports_hdfs(iata) disable novalidate rely;
-alter table flights add constraint flights_new_c2 foreign key (Dest) references airports_hdfs(iata) disable novalidate rely;
-alter table flights add constraint flights_new_c3 foreign key (UniqueCarrier) references airlines_hdfs(code) disable novalidate rely;
-alter table flights add constraint flights_new_c4 foreign key (TailNum) references planes_hdfs(TailNum) disable novalidate rely;
+alter table flights add constraint flights_hdfs_new_c1 foreign key (Origin) references airports_hdfs(iata) disable novalidate rely;
+alter table flights add constraint flights_hdfs_new_c2 foreign key (Dest) references airports_hdfs(iata) disable novalidate rely;
+alter table flights add constraint flights_hdfs_new_c3 foreign key (UniqueCarrier) references airlines_hdfs(code) disable novalidate rely;
+alter table flights add constraint flights_hdfs_new_c4 foreign key (TailNum) references planes_hdfs(TailNum) disable novalidate rely;
 
 ANALYZE TABLE airports_hdfs COMPUTE STATISTICS FOR COLUMNS;
 ANALYZE TABLE airlines_hdfs COMPUTE STATISTICS FOR COLUMNS;
